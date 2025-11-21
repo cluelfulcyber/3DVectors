819ab75df9a916efee5828d033f4fc0f28e458da
cluelfulcyber <rado.gichev08@gmail.com>
2025-11-21 16:26:17 +0200

    Add files via upload

---
+++ 3D_Vector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Numerics;
+class Program
+{
+    static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+        double minLength = double.MaxValue;
+
+        for (int i = 0; i < N; i++)
+        {
+            string[] parts = Console.ReadLine().Split();
+            double x = double.Parse(parts[0]);
+            double y = double.Parse(parts[1]);
+            double z = double.Parse(parts[2]);
+
+            double length = Math.Sqrt(x * x + y * y + z * z);
+
+            if (length < minLength)
+                minLength = length;
+        }
+
+        Console.WriteLine($"nai-kusata dulzhina e: {minLength}");
+    }
+}
